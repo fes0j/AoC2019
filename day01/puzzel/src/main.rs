c826a42896b32a791bc5cdbf78058bc803a86fde
fes0j <fes0j@users.noreply.github.com>
2019-12-01 10:56:58 +0100

    Add BufReader and change gitignore

--- day01/puzzel/src/main.rs
+++ day01/puzzel/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+use std::fs::File;
+
+fn main() -> io::Result<()> {
+    let f = File::open("input")?;
+    let f = BufReader::new(f);
+
+    for line in f.lines() {
+        println!("{}", line.unwrap());
+    }
+
+    Ok(())
 }
