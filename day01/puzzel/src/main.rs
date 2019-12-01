6764aa7e6b7352cdbd21f364068106743e22aa99
fes0j <fes0j@users.noreply.github.com>
2019-12-01 11:09:00 +0100

    day01: part1

--- day01/puzzel/src/main.rs
+++ day01/puzzel/src/main.rs
@@ -1,14 +1,19 @@
 use std::io::{self, BufReader};
 use std::io::prelude::*;
 use std::fs::File;
+use std::ops::Add;
 
 fn main() -> io::Result<()> {
     let f = File::open("input")?;
     let f = BufReader::new(f);
 
+    let mut module_sum : u32 = 0;
     for line in f.lines() {
-        println!("{}", line.unwrap());
+         let mass = line.unwrap().parse::<u32>().unwrap();
+        let fuel = (mass / 3) -2;
+        module_sum = module_sum.add( fuel);
     }
+    println!("Sum: {}", module_sum);
 
     Ok(())
 }
