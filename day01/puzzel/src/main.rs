6f1cddc61a659c8aa13830c804ce3d97eb711ba2
fes0j <fes0j@users.noreply.github.com>
2019-12-01 12:30:14 +0100

    day01: part2

--- day01/puzzel/src/main.rs
+++ day01/puzzel/src/main.rs
@@ -7,13 +7,23 @@ fn main() -> io::Result<()> {
     let f = File::open("input")?;
     let f = BufReader::new(f);
 
-    let mut module_sum : u32 = 0;
+    let mut module_sum: i32 = 0;
+    let mut modules: Vec<i32> = Vec::new();
+
     for line in f.lines() {
-         let mass = line.unwrap().parse::<u32>().unwrap();
-        let fuel = (mass / 3) -2;
-        module_sum = module_sum.add( fuel);
+        let mass = line.unwrap().parse::<i32>().unwrap();
+        modules.push(mass);
+    }
+
+    for mass in modules {
+        module_sum += module2fuel(mass);
     }
     println!("Sum: {}", module_sum);
 
     Ok(())
+}
+
+fn module2fuel(mass: i32) -> i32 {
+    let fuel = (mass / 3) - 2;
+    if fuel > 0 { fuel + module2fuel(fuel) } else { 0 }
 }
