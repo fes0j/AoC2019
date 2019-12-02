cd9fc85f044a28011d481627add6b7d3fffbc723
fes0j <fes0j@users.noreply.github.com>
2019-12-02 17:10:28 +0100

    day02: solve part2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{self, BufReader};
 use std::io::prelude::*;
 use std::fs::File;
+use std::ops::Deref;
 
 fn main() -> io::Result<()> {
     let f = File::open("input")?;
@@ -14,37 +15,63 @@ fn main() -> io::Result<()> {
 
     println!("Read in has {} codes", ops.len());
 
-    ops[1]=12;
-    ops[2]=2;
+    let result1 = part1(ops.clone(),12,2);
+    println!("Part1 result is: {}", result1);
+    let (verb, noun) = part2(ops).expect("Error in loop");
+    let input = 100* noun + verb;
+    println!("Part2: Please put {} into the computer", input);
+
+    Ok(())
+}
+
+fn part2(mut ops : Vec<usize>)-> Result<(usize, usize),()>{
+    let output =19690720;
+
+
+    let mut noun = 0;
+    let mut verb = 0;
+
+
+    for noun  in 0..99 {
+        for verb in 0..99 {
+           if output == part1(ops.clone(),noun,verb){
+               return Ok((verb, noun))
+
+           }
+        }
+    }
+
+    Err(())
+
+
+}
+
+fn part1(mut ops : Vec<usize>, input1 : usize, input2 : usize) -> usize {
+    ops[1]=input1;
+    ops[2]=input2;
     let opcount = ops.len();
     for i in (0..opcount).step_by(4) {
         let opcode = ops[i];
         match opcode {
             1 => {
-                println!("Opcode 1");
                 let target = ops[i + 3];
                 let p1 = ops[i + 1];
                 let p2 = ops[i + 2];
                 ops[target] = ops[p1] + ops[p2];
             }
             2 => {
-                println!("Opcode 2");
                 let target = ops[i + 3];
                 let p1 = ops[i + 1];
                 let p2 = ops[i + 2];
                 ops[target] = ops[p1] * ops[p2];
             }
             99 => {
-                println!("Dead");
                 break;
             }
             _ => println!("Shit hit the fan."),
         }
 
     }
-
-    println!("Result is: {}", ops.get(0).unwrap());
-
-    Ok(())
+    let result =  ops.get(0).unwrap();
+    *result
 }
-
